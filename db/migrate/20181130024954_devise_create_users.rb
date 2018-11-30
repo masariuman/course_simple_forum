70ae126aba9a52d51fda07ddc7082aa61b48b553
masariuman <masariuman@gmail.com>
2018-11-30 10:40:13 +0700

    db/migrate/20181130024954_devise_create_users.rb

--- db/migrate/20181130024954_devise_create_users.rb
+++ db/migrate/20181130024954_devise_create_users.rb
@@ -33,7 +33,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[5.2]
       # t.datetime :locked_at
 
       t.string :name
-      t.integer :levea
+      t.integer :level
 
       t.timestamps null: false
     end
